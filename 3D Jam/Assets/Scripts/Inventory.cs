907779255130615ff084aa8c438ee56af482a81c
Romain "Nebrazif" Habib <romain0habib@gmail.com>
2020-04-06 18:05:45 +0200

    Adding Hover Script & Pickup Script

---
+++ 3D Jam/Assets/Scripts/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] public List<GameObject> playerInventory;
+    [SerializeField] public GameObject playerHand;
+
+    public bool pickupCooldown;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupCooldown = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RefreshInventory();
+    }
+
+    void RefreshInventory()
+    {
+        if(playerInventory.Count == 1)
+        {
+            playerInventory[0].transform.position = playerHand.transform.position;
+            playerInventory[0].transform.parent = playerHand.transform;
+            playerInventory[0].transform.rotation = Quaternion.identity;
+        }
+
+        if(playerInventory.Count == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+            playerInventory[0].transform.position = playerHand.transform.position;
+            playerInventory[0].transform.parent = playerInventory[0].transform.parent;
+            }
+        }
+    }
+
+   public IEnumerator PickupCooldown()
+    {
+        pickupCooldown = true;
+        yield return new WaitForSeconds(1.0f);
+        pickupCooldown = false;
+    }
+}
---
+++ 3D Jam/Assets/Scripts/SelectionFeedback.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionFeedback : MonoBehaviour
+{
+    [Header("Materials Prefs")]
+    [SerializeField] private string selectableTag = "Prop";
+    [SerializeField] private Material normalMaterial;
+    [SerializeField] private Material selectMaterial;
+
+    [SerializeField] private float rayDistance;
+
+    [SerializeField] private Text interactText;
+
+    private Transform selectionTransform;
+
+    [SerializeField] GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Selection();
+    }
+
+
+    void Selection()
+    {
+        if(selectionTransform != null)
+        {
+            var selectionRenderer = selectionTransform.GetComponent<Renderer>();
+            selectionRenderer.material = normalMaterial;
+            selectionTransform = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, rayDistance))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                Debug.Log("Facing : " + hit.transform.name);
+
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = selectMaterial;
+                }
+
+                interactText.text = "Interact : " + hit.transform.name;
+                selectionTransform = selection;
+
+                if (Input.GetKeyDown(KeyCode.E) && !player.GetComponent<Inventory>().pickupCooldown)
+                {
+                    if(player.GetComponent<Inventory>().playerInventory.Count < 1)
+                    {
+                    StartCoroutine(player.GetComponent<Inventory>().PickupCooldown());
+                    Collect(selectionTransform);
+                    }
+                }
+
+            }
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+        }
+        else
+        {
+            interactText.text = "";
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * rayDistance, Color.green);
+        }
+    }
+
+    void Collect(Transform collectable)
+    {
+        player.GetComponent<Inventory>().playerInventory.Add(collectable.gameObject);
+    }
+}
+

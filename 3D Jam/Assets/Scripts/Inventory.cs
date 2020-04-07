58c5f02e4a1be2a0cc7c6851242a599b3b1d9d26
Romain "Nebrazif" Habib <romain0habib@gmail.com>
2020-04-07 09:04:35 +0200

    Adding Release Part to Pickup Script
    Le script est clairement à revoir

--- 3D Jam/Assets/Scripts/Inventory.cs
+++ 3D Jam/Assets/Scripts/Inventory.cs
@@ -9,6 +9,9 @@ public class Inventory : MonoBehaviour
 
     public bool pickupCooldown;
 
+    [SerializeField] private GameObject img1;
+    [SerializeField] private GameObject img2;
+
 
     // Start is called before the first frame update
     void Start()
@@ -35,12 +38,23 @@ public class Inventory : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.R))
             {
-            playerInventory[0].transform.position = playerHand.transform.position;
-            playerInventory[0].transform.parent = playerInventory[0].transform.parent;
+                playerInventory[0].transform.position = playerHand.transform.position;
+                playerInventory[0].transform.parent = playerInventory[0].transform.parent;
+
+                RaycastHit hit = new RaycastHit();
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if(Physics.Raycast(ray, out hit))
+                {
+                    playerInventory[0].transform.parent = GameObject.FindGameObjectWithTag("PropContainer").transform;
+                   // playerInventory[0].transform.position = new Vector3(transform.position.x + 0.5f, 2.0f, transform.position.z + 0.5f);
+                    playerInventory[0].transform.position = new Vector3(transform.position.x, 2, transform.forward.z + 1);
+                }
+
+                playerInventory.Remove(playerInventory[0]);
             }
         }
     }
-
+    
    public IEnumerator PickupCooldown()
     {
         pickupCooldown = true;
